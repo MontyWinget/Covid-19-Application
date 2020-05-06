ea2072140092100513763f70c8b9b70809bf1e2c
Monty Winget <monty.winget@gmail.com>
2020-05-06 13:25:21 +0530

    Country Model

--- app/src/main/java/com/example/covid_19/AffectedCountries.java
+++ app/src/main/java/com/example/covid_19/AffectedCountries.java
@@ -2,13 +2,71 @@ package com.example.covid_19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.leo.simplearcloader.SimpleArcLoader;
+
+import org.eazegraph.lib.models.PieModel;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AffectedCountries extends AppCompatActivity {
+
+    EditText edtSearch;
+    ListView listView;
+    SimpleArcLoader simpleArcLoader;
+
+    public static List<CountryModel> countryModelList = new ArrayList<>();
+    CountryModel countryModel;
+    MyCustomAdapter myCustomAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_affected_countries);
+
+        edtSearch = findViewById(R.id.edtSearch);
+        listView = findViewById(R.id.listView);
+        simpleArcLoader = findViewById(R.id.loader);
+
+        fetchData();
     }
+
+    private void fetchData() {
+        String url = "https://corona.lmao.ninja/v2/all/";
+
+        simpleArcLoader.start();
+
+        StringRequest request = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(AffectedCountries.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(request);
+
+    }
+
 }
--- app/src/main/java/com/example/covid_19/MainActivity.java
+++ app/src/main/java/com/example/covid_19/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.covid_19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -108,5 +109,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void goTrackCountries(View view) {
+
+        startActivity(new Intent(getApplicationContext(), AffectedCountries.class));
     }
 }
